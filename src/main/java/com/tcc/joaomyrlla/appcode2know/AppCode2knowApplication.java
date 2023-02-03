020969ce82da545555761cec332fa2cd76bd136b
João Lima <jpalvesl101@gmail.com>
2023-02-03 18:33:12 -0300

    first commit

---
+++ src/main/java/com/tcc/joaomyrlla/appcode2know/AppCode2knowApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.joaomyrlla.appcode2know;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCode2knowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCode2knowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/joaomyrlla/appcode2know/AppCode2knowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.joaomyrlla.appcode2know;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCode2knowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
